3ff64dc5d80aeb1d068873131f0be3a6d75ca90e
Emma Kirstine Hansen <ekha@itu.dk>
2021-09-17 13:55:02 +0200

    Started implementing Student class

---
+++ StudentLibrary/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StudentLibrary
+{
+    public class Student
+
+    {
+        public Student(int id, string givenName, string surName, DateTime startDate, DateTime endDate, DateTime graduationDate)
+        {
+            Id = id;
+            if (DateTime.Now > startDate && DateTime.Now < endDate)
+            {
+                Status = Status.Active;
+            }
+            else if ()
+        }
+        public int Id { get; }
+        public string GivenName { get; set; }
+        public string SurName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime GraduationDate { get; set; }
+        public Status Status { get; }
+
+    }
+}
+
+public enum Status
+{
+    New,
+    Active,
+    Dropout,
+    Graduated
+}
